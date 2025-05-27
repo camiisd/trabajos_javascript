9c4a05a14f889c67fe79f85b25215c6bfbc4c98c
camiisd <liccamilasoto@gmail.com>
2025-05-26 21:23:28 -0300

    solucionando error anterior

---
+++ Clase9/ej2.js
@@ -0,0 +1,15 @@
+const prompt = require ('prompt-sync')({sigint:true});
+//define dos constantes RANGO_MINIMO y RANGO_MAXIMO
+const rango_Minimo = 2;
+const rango_Maximo = 39;
+//pedir el usuario que ingrese un número
+let numUser = Number(prompt ('Ingrese un número'));
+//verificar si está dentro del rango definido por las constantes
+let mensaje = '';
+if (numUser>rango_Minimo && numUser<rango_Maximo) {
+    mensaje = "está en el rango asignado."
+} else {
+    mensaje = "no está en el rango asignado."
+}
+// mostrar el mensaje
+console.log ('El número ingresado' + mensaje);
