c1be9d2d7985ba285ae55b09f3f4ccdd639504f8
camiisd <liccamilasoto@gmail.com>
2025-05-28 20:01:35 -0300

    Probando el ejercicio 5

--- Clase9/ej5.js
+++ Clase9/ej5.js
@@ -3,4 +3,20 @@ const prompt = require ('prompt-sync')({sigint:true});
 let numUno = prompt ('Ingrese el número a: ');
 let numDos = prompt ('Ingrese el número b: ');
 let numTres = prompt ('Ingrese el número c: ');
-// 
+// determinar cuál es el mayor
+let mensaje = '';
+
+let num1= numUno > numDos && numUno > numTres
+let num2= numDos > numUno && numDos > numTres
+let num3 = numTres > numUno && numTres > numDos
+
+mensaje= ''
+
+if (num1 === true) {
+    mensaje = 'El número mayor es ' + numUno
+} else if (num2 === true) {
+    mensaje = 'El número mayor es ' + numDos
+} else {
+    mensaje = 'El número mayor es ' + numTres
+}
+console.log(mensaje);
