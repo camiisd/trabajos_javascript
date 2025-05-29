4330ff542726cb87c4fd6f5059bf5ed78cfbfe8d
camiisd <liccamilasoto@gmail.com>
2025-05-28 21:14:01 -0300

    Cambie nombre de un archivo

--- ejercicio3.js
+++
@@ -1,8 +0,0 @@
-const prompt = require ('prompt-sync')({sigint:true});
-// el usuario ingresa dos numeros
-let numeroUno = parseFloat(prompt ('Ingrese el primer número: '));
-let numeroDos = parseFloat(prompt ('Ingrese el segundo número:    '));
-//sumar los números
-let suma = numeroUno + numeroDos;
-//mostrar resultado
-console.log('La suma de los dos números es: ' + suma);
--- ejercicio5.js
+++
@@ -1,5 +0,0 @@
-const prompt = require ('prompt-sync')({sigint:true});
-// solicitar al usuario que ingrese su nombre
-let nombre = prompt ('Hola, ¿cómo te llamas? ');
-// mostrar un saludo personalizado usando el nombre ingresado
-console.log ('¡Bienvenid@ ' + nombre + ' a mi GitHub! Espero sorprenderte con mis trabajos prácticos de ADA ITW :)');
