ca21c0b1b09f86b10b149259eecfd137cfca7c41
camiisd <liccamilasoto@gmail.com>
2025-07-05 10:19:48 -0300

    Comienzo de los comentarios en el código (Punto 10)

---
+++ TrabajoIntegrador/CamilaDeniseSoto.js
@@ -0,0 +1,4 @@
+//Sistema de Gestión de Biblioteca
+
+//Punto 10: Comentando mi código
+// Utilizaré las // para comentar mi código, respetando el formato Punto - Consigna - Explicación, o, de ser necesario, también usaré /* */. A excepción del presente para evitar que el mismo sea reiterativo.
