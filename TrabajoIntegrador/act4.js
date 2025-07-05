69ba173ed7a698a61965419a0279b343bbe841fe
camiisd <liccamilasoto@gmail.com>
2025-07-05 10:16:30 -0300

    Modificaciones en la carpeta Trabajo Integrador

--- TrabajoIntegrador/act4.js
+++
@@ -1,6 +0,0 @@
-const prompt = require('prompt-sync')();
-//Ejercicio 4: Calcular descuento por cantidad. Solicita al usuario la cantidad de productos comprados y el precio unitario. Usa una función flecha para calcular el total con descuento según la cantidad.
-let cantidadProductos = parseInt(prompt('Ingrese la cantidad de productos: '));
-let precioUnitario = parseFloat (prompt('Ingrese el valor unitario'));
-
-let 
