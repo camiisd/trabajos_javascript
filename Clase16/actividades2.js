d1b60d7740805b7410c9ac1e4f21afe78694524f
camiisd <liccamilasoto@gmail.com>
2025-07-11 19:33:53 -0300

    Se comparten las resoluciones de los ejercicios 8 a 10 de la Clase 16 en actividades2.js

--- Clase16/actividades2.js
+++ Clase16/actividades2.js
@@ -30,10 +30,28 @@ let nombreEncontrado = nombres.find(nombre => nombre === 'Laura');
 (nombreEncontrado) ? console.log('Está Laura.') : console.log('Laura no está.');
 
 
-//● Ejercicio 9: Cuenta la cantidad total de letras en una lista de palabrasImagina que tienes una lista de palabras ['sol', 'luna', 'estrella']. 
+//● Ejercicio 9: Cuenta la cantidad total de letras en una lista de palabras.Imagina que tienes una lista de palabras ['sol', 'luna', 'estrella']. 
 // Queremos saber cuántas letras hay en total entre todas las palabras. Por ejemplo, 'sol'
 //tiene 3 letras, 'luna' tiene 4, y 'estrella' tiene 8. La suma total debería ser 15.
+
+palabras = ['sol', 'luna', 'estrella'];
+
+let cantidadLetras = palabras.reduce(function (acum, palabra) {
+    palabra = palabra.length
+    acum += palabra
+    return acum;
+}, 0);
+
+console.log(cantidadLetras);
 
 //● Ejercicio 10: Construye una frase a partir de una lista de palabras Dada la lista ['Me', 'gusta', 'aprender', 'JavaScript'], 
 // queremos construir una frase completa concatenando cada palabra en orden, separadas por
 //espacios. Al final, deberíamos obtener el texto 'Me gusta aprender JavaScript'.
+
+palabras = ['Me', 'gusta', 'aprender', 'JavaScript'];
+
+let frase = palabras.reduce((acumulador, palabra) => {
+  return acumulador + ' ' + palabra;
+});
+
+console.log(frase);
